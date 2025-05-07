a0e9e111b7e552e1d67a23c8cf654443f5f40e20
unknown <sadeepaalwis@gmail.com>
2025-05-08 01:01:18 +0530

    updated login analyzer logic

--- services/login_form_analyzer.go
+++ services/login_form_analyzer.go
@@ -1,8 +1,17 @@
 package services
 
+import "strings"
+
 type loginFormAnalyzer struct{}
 
 func (l *loginFormAnalyzer) Analyze(ctx AnalyzerContext) {
+
+	if strings.Contains(strings.ToLower(ctx.InputUrl), "login") {
+
+		ctx.Manager.SetHasLogin(true)
+		return
+	}
+
 	hasLogin := ctx.Document.Find("input[type='password']").Length() > 0
 	ctx.Manager.SetHasLogin(hasLogin)
 }
